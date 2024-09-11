2628d6c88c66b0c78d54295864c182a0c7f7440b
PabloArreaga <paal241199@gmail.com>
2024-09-11 04:29:01 -0600

    ADD: se agrego archivos para trabar con modulos

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -7,19 +7,19 @@ class TaskCreate(BaseModel):
     description: Optional[str] = Field(None, max_length=500)
     created_at: datetime = Field(..., description="La fecha de creación de la tarea, no puede ser una fecha futura")
     deadline: Optional[datetime] = Field(None, description="Debe ser una fecha futura")
-    priority: str = Field(..., regex="^(Baja|Media|Alta)$")
-    status: str = Field(..., regex="^(Pendiente)$")
+    priority: str = Field(..., pattern="^(Baja|Media|Alta)$")
+    status: str = Field(..., pattern="^(Pendiente)$")
     assigned_user: Optional[str] = None
-    category: str = Field(..., regex="^(Administración|Ventas|Soporte|Desarrollo)$")
+    category: str = Field(..., pattern="^(Administración|Ventas|Soporte|Desarrollo)$")
 
 class TaskUpdate(BaseModel):
     title: Optional[str] = Field(None, min_length=5, max_length=100)
     description: Optional[str] = Field(None, max_length=500)
     deadline: Optional[datetime] = Field(None, description="Debe ser una fecha futura")
-    priority: Optional[str] = Field(None, regex="^(Baja|Media|Alta)$")
-    status: Optional[str] = Field(None, regex="^(Pendiente|En Proceso|Completada|Cancelada)$")
+    priority: Optional[str] = Field(..., pattern="^(Baja|Media|Alta)$")
+    status: Optional[str] = Field(None, pattern="^(Pendiente|En Proceso|Completada|Cancelada)$")
     assigned_user: Optional[str] = None
-    category: Optional[str] = Field(None, regex="^(Administración|Ventas|Soporte|Desarrollo)$")
+    category: Optional[str] = Field(None, pattern="^(Administración|Ventas|Soporte|Desarrollo)$")
 
 class TaskResponse(BaseModel):
     id: int
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from app.routers import router
-
+from app.routers.task import router
 app = FastAPI()
 
-# Registrar los routers
+# Registrar el router
 app.include_router(router)
 
-# Punto de entrada para la aplicación
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
